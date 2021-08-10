1eec9cf4e643b0493da4565cbf839b7dafe4d520
roggc <roggc9@gmail.com>
2021-08-10 17:13:54 +0200

    adding test for Range component

---
+++ src/components/Range/Range.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import {Range} from '.'
+
+describe('Range', () => {
+    it('renders', () => {
+      render(<Range  data={{min:1,max:10}} />)
+    });
+  });
