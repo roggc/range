9deaca8c5e5eac916c789c3fe54cdfceb155bf8d
roggc <roggc9@gmail.com>
2021-08-10 16:01:30 +0200

    test modified

--- src/components/NormalRange/NormalRange.test.tsx
+++ src/components/NormalRange/NormalRange.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import {NormalRange} from '.'
+import {QueryClientProvider,QueryClient} from 'react-query'
+
+const queryClient=new QueryClient()
 
 describe('NormalRange', () => {
     it('renders', () => {
-      render(<NormalRange />)
+      render(<QueryClientProvider client={queryClient}><NormalRange /></QueryClientProvider>)
     });
   });
