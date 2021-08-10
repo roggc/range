caf466424f0ad951946f5a0f1dccd4753124ffe5
roggc <roggc9@gmail.com>
2021-08-10 17:11:36 +0200

    extracting Range Component

--- src/components/NormalRange/NormalRange.tsx
+++ src/components/NormalRange/NormalRange.tsx
@@ -1,9 +1,7 @@
 import React,{useRef} from 'react'
 import styled from 'styled-components'
 import {useQuery} from 'react-query'
-import {Bullet} from '../Bullet'
-import {Bar} from '../Bar'
-import {useMoveBullet} from '../../hooks'
+import {Range} from '../Range'
 
 interface INormalRangeProps extends React.HTMLAttributes<HTMLDivElement>{}
 
@@ -14,12 +12,6 @@ export const NormalRange:React.FC<INormalRangeProps>=({...props}):React.ReactEle
      )
    )
 
-    const refMin=useRef<HTMLDivElement>(null)
-    const refMax=useRef<HTMLDivElement>(null)
-    const refBar=useRef<HTMLDivElement>(null)
-    const [isGrabbingMin,activateMin]=useMoveBullet({isMin:true,refBullet:refMin,refOtherBullet:refMax,refBar,range:data?data.max-data.min:undefined})
-    const [isGrabbingMax,activateMax]=useMoveBullet({isMin:false,refBullet:refMax,refOtherBullet:refMin,refBar,range:data?data.max-data.min:undefined})
-
     if (isLoading) return <div>Loading...</div>
         
     // @ts-ignore
@@ -27,69 +19,15 @@ export const NormalRange:React.FC<INormalRangeProps>=({...props}):React.ReactEle
     
     return (
         <NormalRangeContainer {...props}>  
-            <NormalRangeSubContainer> 
-            <Label>{data.min}€</Label>
-            <StyledBar ref={refBar}>
-                <StyledBullet 
-                    onMouseDown={activateMin} 
-                    isGrabbing={isGrabbingMin} 
-                    ref={refMin}
-                />
-                <StyledBullet 
-                    onMouseDown={activateMax} 
-                    isGrabbing={isGrabbingMax} 
-                    ref={refMax}
-                />
-            </StyledBar>
-            <Label>{data.max}€</Label>
-            </NormalRangeSubContainer>
+            <Range data={data} />
         </NormalRangeContainer>
     )
 }
 
-interface INormalRangeContainerProps{}
-
-const NormalRangeContainer=styled.div<INormalRangeContainerProps>`
+const NormalRangeContainer=styled.div`
 height:100%;
 display:flex;
 flex-direction:column;
 justify-content:center;
 align-items:center;
-`
-
-const NormalRangeSubContainer=styled.div`
-display:flex;
-flex-direction:row;
-justify-content:space-between;
-align-items:center;
-`
-
-const Label=styled.div`
-font-family:sans-serif;
-margin:20px;
-user-select:none;
-`
-
-const StyledBar=styled(Bar)`
-display:flex;
-justify-content:space-between;
-`
-
-interface IStyledBulletProps {
-    isGrabbing:boolean;
-}
-
-const StyledBullet=styled(Bullet)<IStyledBulletProps>`
-position:relative;
-top:-5px;
-${({isGrabbing}):string=>`
-cursor:${isGrabbing?'grabbing':'grab'};
-${isGrabbing?'height:20px;width:20px;top:-7px;':''}
-`}
-&:hover{
-    width:20px;
-    height:20px;
-    top:-7px;
-}
-user-select:none;
 `
---
+++ src/components/Range/Range.tsx
@@ -0,0 +1,73 @@
+import React,{useRef} from 'react'
+import styled from 'styled-components'
+import {Bullet} from '../Bullet'
+import {Bar} from '../Bar'
+import {useMoveBullet} from '../../hooks'
+
+interface IRangeProps extends React.HTMLAttributes<HTMLDivElement>{
+    data:any;
+    isFixedValues?:boolean;
+}
+
+export const Range:React.FC<IRangeProps>=({data,isFixedValues=false,...props}):React.ReactElement=>{
+    const refMin=useRef<HTMLDivElement>(null)
+    const refMax=useRef<HTMLDivElement>(null)
+    const refBar=useRef<HTMLDivElement>(null)
+    const [isGrabbingMin,activateMin]=useMoveBullet({isMin:true,refBullet:refMin,refOtherBullet:refMax,refBar,range:data?data.max-data.min:undefined})
+    const [isGrabbingMax,activateMax]=useMoveBullet({isMin:false,refBullet:refMax,refOtherBullet:refMin,refBar,range:data?data.max-data.min:undefined})
+
+    return (
+        <RangeContainer {...props}> 
+            <Label>{data.min}€</Label>
+            <StyledBar ref={refBar}>
+                <StyledBullet 
+                    onMouseDown={activateMin} 
+                    isGrabbing={isGrabbingMin} 
+                    ref={refMin}
+                />
+                <StyledBullet 
+                    onMouseDown={activateMax} 
+                    isGrabbing={isGrabbingMax} 
+                    ref={refMax}
+                />
+            </StyledBar>
+            <Label>{data.max}€</Label>
+        </RangeContainer>
+    )
+}
+
+const RangeContainer=styled.div`
+display:flex;
+flex-direction:row;
+justify-content:space-between;
+align-items:center;
+`
+const Label=styled.div`
+font-family:sans-serif;
+margin:20px;
+user-select:none;
+`
+
+const StyledBar=styled(Bar)`
+display:flex;
+justify-content:space-between;
+`
+
+interface IStyledBulletProps {
+    isGrabbing:boolean;
+}
+
+const StyledBullet=styled(Bullet)<IStyledBulletProps>`
+position:relative;
+top:-5px;
+${({isGrabbing}):string=>`
+cursor:${isGrabbing?'grabbing':'grab'};
+${isGrabbing?'height:20px;width:20px;top:-7px;':''}
+`}
+&:hover{
+    width:20px;
+    height:20px;
+    top:-7px;
+}
+user-select:none;
+`
